25b3d43a84b996824e0535734756b2893fa51e21
HarshitaS01 <87194955+codeharshita@users.noreply.github.com>
2022-07-10 22:06:23 +0530

    Update listgroup.jsx

--- src/components/common/listgroup.jsx
+++ src/components/common/listgroup.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import { genres } from '../../services/fakeGenreService';
+import React from "react";
 
-const Listgroup = (props) => {
-
-    const {items, textProperty, valueProperty}=props; //Object destructuring.
-
-
-    return <ul className="list-group">
-        {items.map( item => 
-        <li key={item[valueProperty]} className="list-group-item">
-            {item[textProperty]}
-         </li> )}
-        
-    </ul>;
+const ListGroup = ({
+  items,
+  textProperty,
+  valueProperty,
+  selectedItem,
+  onItemSelect
+}) => {
+  return (
+    <ul className="list-group">
+      {items.map(item => (
+        <li
+          onClick={() => onItemSelect(item)}
+          key={item[valueProperty]}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
+        >
+          {item[textProperty]}
+        </li>
+      ))}
+    </ul>
+  );
 };
- 
-export default Listgroup;
+
+ListGroup.defaultProps = {
+  textProperty: "name",
+  valueProperty: "_id"
+};
+
+export default ListGroup;
