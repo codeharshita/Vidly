e528c70014940b8010cb3276bd156a8361563e6d
HarshitaS01 <87194955+codeharshita@users.noreply.github.com>
2022-07-10 22:10:59 +0530

    Update navbar.jsx

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Link , NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+
 
 const Navbar = () => {
+    
     return ( 
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
     <Link className="navbar-brand" to="/">
@@ -28,6 +30,13 @@ const Navbar = () => {
         <NavLink className="nav-item nav-link" to="/rentals">
             Rentals
             </NavLink>
+            <NavLink className="nav-item nav-link" to="/register">
+           Register
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/login">
+            Login
+            </NavLink>
+        
         
         </div>
     </div>
